0b862ab0d3a762bb0600193479f90e5d054cffc0
Vivek Galatage <vivekgalatage@gmail.com>
2014-09-15 19:53:00 +0530

    Create Console.js

---
+++ src/js/Console.js
@@ -0,0 +1,27 @@
+function Console() {
+
+}
+
+Console.prototype = {
+    LOG: 0,
+    INFO: 1,
+    WARN: 2,
+    ERROR:3,
+    log: function(message) {
+        print(this.LOG, message);
+    },
+
+    info: function(message) {
+        print(this.INFO, message);
+    },
+
+    warn: function(message) {
+        print(this.WARN, message);
+    },
+
+    error: function(message) {
+        print(this.ERROR, message);
+    }
+}
+
+console = new Console;

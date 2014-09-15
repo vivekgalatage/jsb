9dc7246ab1f655f9c76fcf1c5df365ba78abfef7
Vivek Galatage <vivekgalatage@gmail.com>
2014-09-15 19:49:55 +0530

    Update js2string.py

--- tools/scripts/js2string.py
+++ tools/scripts/js2string.py
@@ -6,6 +6,8 @@ import sys
 def main():
     output_filename = sys.argv[1]
     input_filenames = sys.argv[2:]
+    contents = []
+
     for input_filename in input_filenames:
         class_name, ext = os.path.splitext(os.path.basename(input_filename))
         with open(input_filename) as input_file:
@@ -13,6 +15,9 @@ def main():
             hex_values = ['0x{0:02x}'.format(ord(char)) for char in input_text]
             contents.append('const unsigned char kSourceOf%s[] = {\n    %s\n};\n\n' % (
                 class_name, ', '.join(hex_values)))
+    output = open(output_filename, "w")
+    output.write("".join(contents))
+    output.close()
                 
 if __name__ == '__main__':
     sys.exit(main())

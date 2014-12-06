3a26611a97ab6270455b6a4e44fed2cd182c44be
Vivek Galatage <vivek.vg@samsung.com>
2014-12-07 01:16:41 +0530

    Need to call explicit V8::Initialize() as per https://code.google.com/p/v8/issues/detail?id=3627"
    See https://code.google.com/p/v8/source/detail?r=24067 for more details.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
-/* 
+/*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014 Vivek Galatage
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
+ * SOFTWARE.
  */
 
 #include "generated.h"
@@ -136,6 +136,7 @@ int main(int argc, char* argv[])
         printf("Print Usage\n");
         return 0;
     }
+    V8::Initialize();
     isolate = Isolate::New();
     Isolate::Scope isolate_scope(isolate);
     HandleScope handle_scope(isolate);
@@ -169,7 +170,7 @@ int main(int argc, char* argv[])
     Handle<Value> result;
 
     result = script->Run();
-    
+
     return 0;
 }
 
